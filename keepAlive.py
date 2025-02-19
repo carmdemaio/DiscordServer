5beedbacb2d978717be00be851069410d9bcb24e
Carm <rividz@gmail.com>
2025-02-19 12:34:28 -0800

    Update keepAlive.py
    setup to not have to run on Gunicorn, a production-grade WSGI server. Edited line 14 + 15

--- keepAlive.py
+++ keepAlive.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask('')
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "I'm alive!"
 
+
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    #app.run(host='0.0.0.0', port=8080)   Use this if you want to run on testing on replit
+    os.system("gunicorn -b 0.0.0.0:8080 keepAlive:app --log-file -")
+
 
 def keep_alive():
     t = Thread(target=run)
